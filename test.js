4eb84eea35a40ee5035253061e2937a28219f895
new45605 <dev.acc.nine.nine@gmail.com>
2025-02-15 08:32:21 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-async function fetchTime() {
-  try {
-    let response = await fetch('https://worldtimeapi.org/api/timezone/Etc/UTC');
-    let data = await response.json();
-    console.log('Current UTC Time:', data.datetime);
-  } catch (error) {
-    console.error('Error fetching time:', error);
-  }
-}
 
-fetchTime();
